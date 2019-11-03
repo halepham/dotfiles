7b715d4331f18c236804f26b5d4a7220ce2bdf2d
Hale Pham <phamanhtu19297@gmail.com>
2019-11-04 00:48:10 +0700

    install.sh: Create backup files before install

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Backup files before install
+if [ ! -d ./bkp ]
+then
+    mkdir bkp
+fi
+
+if [ -f ~/.vimrc ]
+then
+    cp ~/.vimrc "./bkp/vimrc_$(date '+%Y%m%d%H%M%S')"
+    cp ~/.bashrc "./bkp/bashrc_$(date '+%Y%m%d%H%M%S')"
+fi
+
 cp ./vimrc ~/.vimrc
 
 if [ ! -d ~/.dotfiles ]
@@ -8,4 +20,4 @@ then
 fi
 cp -r ./shell ./tbw.sh ~/.dotfiles
 
-cat ./bashrc > ~/.bashrc
+cat ./bashrc >> ~/.bashrc
