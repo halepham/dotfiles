9c1be95d6c841f8703318a0289bebb2c8b56f766
Tu Pham <phamanhtu19297@gmail.com>
2019-11-26 23:50:40 +0700

    install.sh: small update for install.sh
    Signed-off-by: Tu Pham <phamanhtu19297@gmail.com>

--- install.sh
+++ install.sh
@@ -1,23 +1,35 @@
 #!/bin/bash
 
-# Backup files before install
-if [ ! -d ./bkp ]
+# Create dir
+mkdir -p $HOME/.dotfiles/bkp/
+mkdir -p $HOME/.dotfiles/shell/
+
+SCRIPT_PATH=$(dirname $0)
+DOTFILE=$HOME/.dotfiles
+BKP=$DOTFILE/bkp
+SHELL=$DOTFILE/shell
+
+# Backup files before update.
+[ -f $HOME/.vimrc ] && cp ~/.vimrc "$BKP/vimrc_$(date '+%Y%m%d%H%M%S')"
+
+[ -f $HOME/.config/nvim/init.vim ] && \
+    cp $HOME/.config/nvim/init.vim "$BKP/init_vim_$(date '+%Y%m%d%H%M%S')"
+
+[ -f $HOME/.bashrc ] && cp $HOME/.bashrc "$BKP/bashrc_$(date '+%Y%m%d%H%M%S')"
+
+[ -f $HOME/.inputrc ] && cp $HOME/.inputrc "$BKP/inputrc_$(date '+%Y%m%d%H%M%S')"
+
+#Install
+cp $SCRIPT_PATH/shell/* $SHELL
+cp $SCRIPT_PATH/bashrc $HOME/.bashrc
+cp $SCRIPT_PATH/vimrc $HOME/.vimrc
+cp $SCRIPT_PATH/inputrc $HOME/.inputrc
+
+if [ -f $HOME/.config/nvim/init.vim ]
 then
-    mkdir bkp
+    cp $SCRIPT_PATH/vimrc $HOME/.config/nvim/init.vim
+else
+    mkdir -p $HOME/.config/nvim
+    cp $SCRIPT_PATH/vimrc $HOME/.config/nvim/init.vim
 fi
 
-if [ -f ~/.vimrc ]
-then
-    cp ~/.vimrc "./bkp/vimrc_$(date '+%Y%m%d%H%M%S')"
-    cp ~/.bashrc "./bkp/bashrc_$(date '+%Y%m%d%H%M%S')"
-fi
-
-cp ./vimrc ~/.vimrc
-
-if [ ! -d ~/.dotfiles ]
-then
-    mkdir ~/.dotfiles
-fi
-cp -r ./shell ./tbw.sh ~/.dotfiles
-
-cat ./bashrc >> ~/.bashrc
