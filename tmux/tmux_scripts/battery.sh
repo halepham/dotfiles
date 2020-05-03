c8add39509035241c3ae151466bd1a7207cd8fbe
Tu Pham <phamanhtu19297@gmail.com>
2020-05-03 21:15:06 +0700

    add configure for tmux.
    Signed-off-by: Tu Pham <phamanhtu19297@gmail.com>

---
+++ tmux/tmux_scripts/battery.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+DIR="/sys/class/power_supply/BAT0"
+charge_full=$( cat $DIR/charge_full )
+echo "$charge_full"
+charge_now=$( cat $DIR/charge_now )
+echo "$charge_now"
+current_now=$( cat $DIR/current_now )
+echo "$current_now"
+batt_status=$( cat $DIR/status )
+echo "$batt_status"
+
+function battery_life() {
+    batt_per=$( echo "scale=1;$charge_now * 100 / $charge_full" | bc )
+    reset_color="#[fg=colour252, bg=colour235, nobold]"
+    if (( $( echo "$batt_per > 80" | bc ) )); then
+        color="#[fg=colour46, bg=colour235, nobold]" # green
+    elif (( $( echo "$batt_per > 40" | bc ) )); then
+        color="#[fg=colour226, bg=colour235, nobold]" # yellow
+    elif (( $( echo "$batt_per > 20" | bc ) )); then
+        color="#[fg=colour214, bg=colour235, nobold]" # orange
+    else
+        color="#[fg=colour196, bg=colour235, nobold]" # red
+    fi
+
+    # Display battery life
+    num=$( echo "$batt_per / 20" | bc )
+    graph="${reset_color}[${color}"
+    for i in {1..5}; do
+        if [[ $i -le $num ]]; then
+            graph+="|"
+        else
+            graph+=" "
+        fi
+    done
+    graph+="${reset_color}]${color}"
+    printf '%s %4.1f%%%s' "${graph}" "$batt_per" "${reset_color}"
+}
+
+function convert_time() {
+    batt_time=$1
+    sec=$( echo "$batt_time % 60" | bc )
+    min=$( echo "$batt_time / 60 % 60" | bc )
+    hour=$( echo "$batt_time / 3600" | bc )
+
+    if [[ $hour -gt 0 ]]; then
+        printf '%d:%02d left' "$hour" "$min"
+    elif [[ $min -gt 0 ]]; then
+        printf '%d mins left' "$min"
+    else
+        printf '<1 min left'
+    fi
+}
+
+function battery_time() {
+    if [[ $current_now -gt 1000 ]]; then
+        if [[ $batt_status == "Charging" ]]; then
+            batt_time=$(echo "($charge_full-$charge_now)*3600/$current_now"| bc)
+        elif [[ $batt_status == "Discharging" ]]; then
+            batt_time=$( echo "$charge_now * 3600 / $current_now"| bc )
+        fi
+        printf "$batt_status $( convert_time $batt_time )"
+    else
+        batt_life=$(battery_life)
+        if [[ $batt_life -eq 100 ]]; then
+            batt_status="Fully-charged"
+        else
+            batt_status="Estimating..."
+        fi
+        printf "$batt_status"
+    fi
+
+}
+
+printf '%s %s ' "$(battery_life)" "$(battery_time)"
---
+++ tmux/tmux_scripts/mem_cpu_load.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+# CPU
+function get_cpu_usage() {
+    grep "$1" /proc/stat |\
+    awk '{idle=($5+$6);total=($2+$3+$4+$5+$6+$7+$8+$9);\
+          printf idle " " total}'
+}
+function cpu() {
+    # Get prevous CPU usage
+    pre=( $( get_cpu_usage "$1" ) )
+
+    sleep 1
+ 
+    # Get current CPU usage
+    cur=( $( get_cpu_usage "$1" ) )
+
+    # Calculating CPU usage percentage
+    let idle=cur[0]-pre[0]
+    let total=cur[1]-pre[1]
+    cpu_usage=$( echo "scale=1;($total - $idle) * 100 / $total" | bc )
+    echo $cpu_usage
+}
+
+function cpu_graph() {
+    usage=$( cpu 'cpu ')
+    reset_color="#[fg=colour252, bg=colour235, nobold]"
+    if (( $( echo "$usage < 20" | bc ) )); then
+        color="#[fg=colour46, bg=colour235, nobold]" # green
+    elif (( $( echo "$usage < 50" | bc ) )); then
+        color="#[fg=colour226, bg=colour235, nobold]" # yellow
+    elif (( $( echo "$usage < 80" | bc ) )); then
+        color="#[fg=colour214, bg=colour235, nobold]" # orange
+    else
+        color="#[fg=colour196, bg=colour235, nobold]" # red
+    fi
+
+    # Display CPU
+    num=$( echo "$usage / 10" | bc )
+    graph="${reset_color}[${color}"
+    for i in {1..10}; do
+        if [[ $i -le $num ]]; then
+            graph+="|"
+        else
+            graph+=" "
+        fi
+    done
+    graph+="${reset_color}]${color}"
+    printf '%s %4.1f%%%s' "${graph}" "$usage" "${reset_color}"
+}
+
+# Memmory
+function memmory() {
+    mem_total=$( cat /proc/meminfo | grep '^MemTotal:' | grep -oP '[0-9]+' )
+    mem_free=$( cat /proc/meminfo | grep '^MemFree:' | grep -oP '[0-9]+' )
+    buffers=$( cat /proc/meminfo | grep '^Buffers:' | grep -oP '[0-9]+' )
+    cached=$( cat /proc/meminfo | grep '^Cached:' | grep -oP '[0-9]+' )
+    swap_cached=$( cat /proc/meminfo | grep '^SwapCached:' | grep -oP '[0-9]+' )
+
+    # Calculating memmory used
+    mem_used=$( echo "$mem_total - $mem_free" | bc )
+    mem_used=$( echo "$mem_used - $buffers - $cached - $swap_cached" | bc ) 
+
+    # Convert to MB
+    mem_total=$( echo "$mem_total/1024" | bc )
+    mem_used=$( echo "$mem_used/1024" | bc )
+
+    # Display memmory
+    reset_color="#[fg=colour252, bg=colour235, nobold]"
+    if (( $( echo "$mem_used * 100 /$mem_total < 20" | bc ) )); then
+        color="#[fg=colour46, bg=colour235, nobold]" # green
+    elif (( $( echo "$mem_used * 100 /$mem_total < 50" | bc ) )); then
+        color="#[fg=colour226, bg=colour235, nobold]" # yellow
+    elif (( $( echo "$mem_used * 100 /$mem_total < 80" | bc ) )); then
+        color="#[fg=colour214, bg=colour235, nobold]" # orange
+    else
+        color="#[fg=colour196, bg=colour235, nobold]" # red
+    fi
+    echo "${color}$mem_used${reset_color}/${color}$mem_total${reset_color}MB"
+}
+
+# Load average
+function load_average()
+{
+    load=$( uptime | grep -oP '(?<=average: )[0-9., ]+' | sed 's/,//g' )
+    echo "#[fg=colour44, bg=colour235, nobold]$load"
+}
+
+printf ' %s %s %s' "$(memmory)" "$(cpu_graph)" "$(load_average)"
