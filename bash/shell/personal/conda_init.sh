069bbf4328d26f8f14d04ec882f03f2292526ed4
Tu Pham <phamanhtu19297@gmail.com>
2020-06-03 01:12:28 +0700

    bash: rename aliases, profile, prompt scripts.
    Signed-off-by: Tu Pham <phamanhtu19297@gmail.com>

---
+++ bash/shell/personal/conda_init.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# >>> conda init >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$(CONDA_REPORT_ERRORS=false '/opt/anaconda3/bin/conda' shell.bash hook 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/anaconda3/etc/profile.d/conda.sh"
+        CONDA_CHANGEPS1=false conda activate base
+    else
+        export PATH="/opt/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda init <<<
