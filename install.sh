13a394ea7e1e5941cf6c944cb9c0ed80b4c56bae
Tu Pham <phamanhtu19297@gmail.com>
2020-06-03 01:14:27 +0700

    add install script.
    Signed-off-by: Tu Pham <phamanhtu19297@gmail.com>

--- install.sh
+++ install.sh
@@ -1,40 +1,135 @@
 #!/bin/bash
 
-# Create dir
-mkdir -p $HOME/.dotfiles/bkp/
-mkdir -p $HOME/.dotfiles/shell/
-
 SCRIPT_PATH=$(dirname $0)
-DOTFILE=$HOME/.dotfiles
-BKP=$DOTFILE/bkp
-SHELL=$DOTFILE/shell
+BKP=$SCRIPT_PATH/backup
+BASH_DIR=$HOME/.bash
+TMUX_DIR=$HOME/.tmux
+SCRIPT_DIR=$HOME/.script
+NVIM_PATH=$(which nvim)
+TMUX_VER=$(tmux -V | sed -En "s/^tmux ([0-9]+(.[0-9]+)?).*/\1/p")
 
-# Backup files before update.
-[ -f $HOME/.vimrc ] && cp ~/.vimrc "$BKP/vimrc_$(date '+%Y%m%d%H%M%S')"
+function backup() {
+    echo Starting backup files at $(date)
+    DATE=$(date '+%Y%m%d%H%M%S')
 
-[ -f $HOME/.config/nvim/init.vim ] && \
-    cp $HOME/.config/nvim/init.vim "$BKP/init_vim_$(date '+%Y%m%d%H%M%S')"
-[ -f $HOME/.config/nvim/coc-settings.json ] && \
-    cp $HOME/.config/nvim/coc-settings.json "$BKP/coc-settings_$(date '+%Y%m%d%H%M%S')"
+    # Create backup dir
+    mkdir -p $SCRIPT_PATH/backup/
 
-[ -f $HOME/.bashrc ] && cp $HOME/.bashrc "$BKP/bashrc_$(date '+%Y%m%d%H%M%S')"
+    [[ -f $HOME/.inputrc ]] && cp $HOME/.inputrc $BKP/inputrc_$DATE
+    [[ -f $HOME/.config/nvim/init.vim ]] && cp $HOME/.config/nvim/init.vim $BKP/init.vim_$DATE
+    [[ -f $HOME/.vimrc ]] && cp $HOME/.vimrc $BKP/vimrc_$DATE
+    [[ -f $HOME/.vim/coc-settings.json ]] && cp $HOME/.vimrc $BKP/vim_coc-settings.json_$DATE
+    [[ -f $HOME/.config/nvim/coc-settings.json ]] && cp $HOME/.vimrc $BKP/nvim_coc-settings.json_$DATE
+    [[ -f $HOME/.tmux.conf ]] &&cp $HOME/.tmux.conf $BKP/tmux.conf_$DATE
+    echo Finish backup files
+}
 
-[ -f $HOME/.inputrc ] && cp $HOME/.inputrc "$BKP/inputrc_$(date '+%Y%m%d%H%M%S')"
+function install_bash() {
+    # Create bash dir
+    mkdir -p $HOME/.bash/
+    echo Seting up bash scripts
+    read -p "Install for personal or work: " res
+    while ! [[ $res =~ ^(personal|work)$ ]]; do
+        read -p "Input 'personal' or 'work' only: " res
+    done
 
-#Install
-cp $SCRIPT_PATH/shell/* $SHELL
-cp $SCRIPT_PATH/vimrc $HOME/.vimrc
-cp $SCRIPT_PATH/inputrc $HOME/.inputrc
+    cp $SCRIPT_PATH/bash/inputrc $HOME/.inputrc
+    if [[ $res == "personal" ]]; then
+        # Create script dir
+        mkdir -p $HOME/.script/
+        cp $SCRIPT_PATH/script/* $SCRIPT_DIR/
 
-if [ ! "$( cat $HOME/.bashrc | grep \"\# My dotfiles\" )" ]
-then
-    cat $SCRIPT_PATH/bashrc >> $HOME/.bashrc
-fi
+        cat $SCRIPT_PATH/bash/bashrc >> $HOME/.bashrc
+        echo 'eval "$(register-python-argcomplete3 conda)"' >> $HOME/.bashrc
+        cp $SCRIPT_PATH/bash/shell/personal/* $BASH_DIR/
+    else
+        cat $SCRIPT_PATH/bash/bashrc >> $HOME/.bashrc
+        cp $SCRIPT_PATH/bash/shell/work/* $BASH_DIR/
+    fi
+    echo Finish setup bash scripts
+}
 
-if [ -f $HOME/.config/nvim/init.vim ]
-then
-    cp $SCRIPT_PATH/{vimrc, coc-settings.json} $HOME/.config/nvim
+function install_vim() {
+    # Check nvim
+    echo Setting up Vim
+    if [[ -z $NVIM_PATH ]]; then
+        read -p "Using plugins? (yes, no): " res
+        while ! [[ $res =~ ^(yes|no)$ ]]; do
+            read -p "Input 'yes' or 'no' only: " res
+        done
+
+        if [[ $res == "yes" ]]; then
+            # Create vim dir
+            mkdir -p $HOME/.vim/
+            cp $SCRIPT_PATH/vim/has_plugin/vimrc $HOME/.vimrc
+            cp $SCRIPT_PATH/vim/has_plugin/coc-settings.json $HOME/.vim/
+        else
+            mkdir -p $HOME/.vim/colors
+            cp $SCRIPT_PATH/vim/no_plugin/vimrc $HOME/.vimrc
+            cp $SCRIPT_PATH/vim/no_plugin/edge.vim $HOME/.vim/colors/
+        fi
+    else
+        read -p "Using vim or neovim? " res
+        while ! [[ $res =~ ^(vim|neovim)$ ]]; do
+            read -p "Input 'vim' or 'neovim' only: " res
+        done
+        
+        read -p "Using plugins? (yes, no): " plugin
+        while ! [[ $plugin =~ ^(yes|no)$ ]]; do
+            read -p "Input 'yes' or 'no' only: " plugin
+        done
+
+        if [[ $res == "vim" ]]; then
+            if [[ $plugin == "yes" ]]; then
+                # Create vim dir
+                mkdir -p $HOME/.vim/
+                cp $SCRIPT_PATH/vim/has_plugin/vimrc $HOME/.vimrc
+                cp $SCRIPT_PATH/vim/has_plugin/coc-settings.json $HOME/.vim/
+            else
+                mkdir -p $HOME/.vim/colors
+                cp $SCRIPT_PATH/vim/no_plugin/vimrc $HOME/.vimrc
+                cp $SCRIPT_PATH/vim/no_plugin/edge.vim $HOME/.vim/colors/
+            fi
+        else
+            if [[ $plugin == "yes" ]]; then
+                # Create nvim dir
+                mkdir -p $HOME/.config/nvim
+                cp $SCRIPT_PATH/vim/has_plugin/vimrc $HOME/.config/nvim/init.vim
+                cp $SCRIPT_PATH/vim/has_plugin/coc-settings.json $HOME/.config/nvim/coc-settings.json
+            else
+                mkdir -p $HOME/.config/nvim/colors
+                cp $SCRIPT_PATH/vim/no_plugin/vimrc $HOME/.config/nvim/init.vim
+                cp $SCRIPT_PATH/vim/no_plugin/edge.vim $HOME/.config/nvim/colors/
+            fi
+        fi
+    fi
+    echo Finish setup Vim
+}
+
+function install_tmux() {
+    echo Setting up Tmux
+    # Create tmux dir
+    mkdir -p $HOME/.tmux/scripts/../themes
+
+    # Checking tmux version
+    if [[ "$(echo "$TMUX_VER == 1.8" | bc)" == 1 ]]; then
+        cp $SCRIPT_PATH/tmux/v1.8/.tmux.conf $HOME/.tmux.conf
+    elif [[ "$(echo "$TMUX_VER >= 2.1" | bc)" == 1 ]]; then
+        cp $SCRIPT_PATH/tmux/latest/.tmux.conf $HOME/.tmux.conf
+        cp $SCRIPT_PATH/tmux/scripts/* $TMUX_DIR/scripts/
+        cp $SCRIPT_PATH/tmux/themes/* $TMUX_DIR/themes/
+    else
+        echo "Tmux not found!"
+    fi
+    echo Finish setup Tmux
+}
+
+# Main
+if grep "\# dotfile - Start" $HOME/.bashrc > /dev/null ; then
+    echo "You have already installed!"
 else
-    mkdir -p $HOME/.config/nvim
-    cp $SCRIPT_PATH/{vimrc, coc-settings.json} $HOME/.config/nvim
+    backup
+    install_bash
+    install_vim
+    install_tmux
 fi
