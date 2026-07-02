bac05d92138027b8bf3be6fb92b3423c33521297
Tu Pham <tu.pham.ct@renesas.com>
2026-07-02 09:10:50 +0700

    vim: yanks with system clipboard.
    Signed-off-by: Tu Pham <tu.pham.ct@renesas.com>

--- vim/lua/config/options.lua
+++ vim/lua/config/options.lua
@@ -2,5 +2,7 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 if vim.g.vscode then
-    vim.opt.cmdheight = 50000  -- Change to 2 or 3 if the panel still pops up
+  vim.opt.cmdheight = 50000 -- Change to 2 or 3 if the panel still pops up
+else
+  vim.opt.clipboard = "unnamedplus"
 end
